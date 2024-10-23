a55659e66c038a78b90c2655903068e57c850099
KobeBerckmans <kobe.berckmans@student.ehb.be>
2024-10-23 14:44:01 +0200

    Initial Commit

---
+++ VideoEditing/VideoEditingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditingApp.swift
+//  VideoEditing
+//
+//  Created by Kobe Berckmans on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
